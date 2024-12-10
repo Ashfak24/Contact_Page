f6f518c58da0f7aa86322a449a06106af165cf1f
Ashfak24 <ashfaksylbd@gmail.com>
2024-12-11 02:05:23 +0600

    Updated project

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,24 +7,45 @@ import { HiOutlineMailOpen } from "react-icons/hi";
 function ContactForm() {
   return (
     <section className={styles.container}>
-      <div className={styles.contact_form}></div>
-      <div className={styles.top_btn}>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<RiMessage2Line fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<FiPhoneCall fontSize="24px" />} />
+        </div>
         <Button
-          text="VIA SUPPORT CHAT"
-          icon={<RiMessage2Line fontSize="24px" />}
+          isOutLine={true}
+          text="VIA EMAIL FORM"
+          icon={<HiOutlineMailOpen fontSize="24px" />}
         />
-        <Button text="VIA CALL" icon={<FiPhoneCall fontSize="24px" />} />
+        <form>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Text</label>
+            <textarea name="text" rows="8" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+            }}
+          >
+            <Button text="SUBMIT" />
+          </div>
+        </form>
       </div>
-      <Button
-        isOutLine={true}
-        text="VIA EMAIL FORM"
-        icon={<HiOutlineMailOpen fontSize="24px" />}
-      />
-      <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" />
-      </form>
-      <div className={styles.contact_image}></div>
+      <div className={styles.contact_image}>
+        <img src="./images/service.png" alt="contact image" />
+      </div>
     </section>
   );
 }
